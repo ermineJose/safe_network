93baa9d4879d44ec1bdd64685d424b0f2afd0e1d
Benno Zeeman <bzeeman@live.nl>
2024-10-02 16:07:20 +0200

    test(autonomi): add 'get' test for wasm

--- autonomi/tests/wasm.rs
+++ autonomi/tests/wasm.rs
@@ -50,3 +50,51 @@ async fn file() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+#[allow(clippy::unwrap_used)]
+#[wasm_bindgen_test]
+async fn fetch() -> Result<(), Box<dyn std::error::Error>> {
+    console_error_panic_hook::set_once();
+
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_ansi(false) // Only partially supported across browsers
+        .without_time() // std::time is not available in browsers
+        .with_writer(tracing_web::MakeWebConsoleWriter::new()); // write events to the console
+    tracing_subscriber::registry()
+        .with(fmt_layer)
+        .with(tracing_subscriber::EnvFilter::new(
+            // Enable logging for crates and this file
+            "sn_networking,autonomi,wasm",
+        ))
+        .init();
+
+    tracing::info!("Starting test");
+
+    let peers = vec![
+        "/ip4/127.0.0.1/tcp/41135/ws/p2p/12D3KooWKPW8e4epFe6AWHuTKoWYGpq2egaCzbNNa6kzbBp6D6w6"
+            .try_into()
+            .expect("str to be valid multiaddr"),
+    ];
+
+    let rpc_url = option_env!("RPC_URL").ok_or("")?;
+    let payment_token_address = option_env!("PAYMENT_TOKEN_ADDRESS").ok_or("")?;
+    let chunk_payments_address = option_env!("CHUNK_PAYMENTS_ADDRESS").ok_or("")?;
+
+    let network = Network::Custom(CustomNetwork::new(
+        rpc_url,
+        payment_token_address,
+        chunk_payments_address,
+    ));
+
+    let mut client = Client::connect(&peers).await.unwrap();
+    tracing::info!("CONNECTED");
+    let wallet = evm_wallet_from_env_or_default(network);
+
+    let addr = autonomi::client::address::str_to_xorname(
+        "6425926e2044f3eacbbc3d4d34316295ac8b7e8ad753d99c358a7bf66d778d94",
+    )?;
+    // let addr = autonomi::XorName::from_content(&[0]);
+    let data_fetched = client.get(addr).await.unwrap();
+
+    Ok(())
+}
